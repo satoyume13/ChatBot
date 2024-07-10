2733c81474cfc82ffe3b415f85035eea216a9414
satoyume13 <90615453+satoyume13@users.noreply.github.com>
2024-07-10 19:26:12 +0000

    new

--- llm_chains.py
+++ llm_chains.py
@@ -12,7 +12,7 @@ from langchain_community.llms import CTransformers
 from langchain_community.vectorstores import Chroma
 from langchain_community.llms import Ollama
 from operator import itemgetter
-from utils import load_config
+from  modules.utils import load_config
 import chromadb
 import yaml
 
