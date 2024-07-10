5d50846853620c97ecfbcc25b714d2f015aa264b
satoyume13 <90615453+satoyume13@users.noreply.github.com>
2024-07-10 18:08:57 +0000

    new

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+from langchain.chains.loading import _load_llm_chain
+from langchain_community.embeddings import llm_rails
 import streamlit as st 
+from llm_chains import load_normal_chain
+from langchain.memory import StreamlitChatMessageHistory
+
+def load_chain(chat_history):
+    return load_normal_chain(chat_history)
+      
 
 def clear_input_field():
     st.session_state.user_qution = st.session_state.user_input
@@ -17,16 +25,21 @@ def main():
         st.session_state.send_input= False
         st.session_state.user_qution = ''
 
+    chat_history= StreamlitChatMessageHistory(key= 'history')
+    llm_chain = load_chain(chat_history)
+
     user_input = st.text_input('type your message here', key='user_input', on_change =set_send_input)
 
     send_button = st.button('Send', key='send_button')
 
     if send_button or st.session_state.send_input:
         if st.session_state.user_qution != '':
-            llm_response = 'This is a response from LLM model'
+            
             with chat_container:
                st.chat_message('user').write(st.session_state.user_qution)   
-               st.chat_message('ai').write('here is an answer')
+               llm_response = llm_chain.run(st.session_state.user_qution)
+               st.chat_message('ai').write(llm_response)
+               if st.session_state.user_qution = '':
 
 if __name__ == '__main__':
     main()
---
+++ llm_chains.py
@@ -0,0 +1,53 @@
+from streamlit import stop
+from prompt_templates import memory_prompt_template
+from typing import Self
+from langchain import memory
+from langchain.chains import LLMChain
+from langchain.chains.retrieval_qa.base import RetrievalQA
+from langchain_community import chat_message_histories
+from langchain_community.embeddings import HuggingFaceInstructEmbeddings
+from langchain.memory import ConversationBufferWindowMemory, chat_memory
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain_community.vectorstores import Chroma
+from langchain_community.llms import Ollama
+from operator import itemgetter
+from utils import load_config
+import chromadb
+import yaml
+
+
+with open('./config.yyaml','r') as f:
+    config = yaml.safe_load(f)
+  
+def create_llm(model_path = config['model_path']['large'], model_type = config['model_path'],model_config= 
+ config['model_config']):
+  llm = CTransformers(model=model_path,model_type= model_type,config= model_config)
+  return llm
+
+def create_embeddings(embeddings_path = config['embeddings_path']):
+  return HuggingFaceInstructEmbeddings(embeddings_path) 
+
+def create_chat_memory(chat_history):
+  return ConversationBufferWindowMemory(memory_key= 'history', chat_memory= chat_history, k=3)
+
+
+def create_prompt_from_template(template):
+  return PromptTemplate.from_template(template)
+  
+def create_llm_chain(llm, chat_prompt, memory):
+  return LLMChain(llm= llm, prompt= chat_prompt,memory= memory)
+  
+def create_chain(chat_history):
+  return chatChain(chat_history)
+
+ 
+class chatChain:
+  
+  def __init__(self, chat_history):
+     self.memory = create_chat_memory(chat_history)
+     llm = create_llm()
+     chat_prompt = create_prompt_from_template(memory_prompt_template)
+     self.llm_chain = create_llm_chain(llm, chat_prompt, self.memory)
+  def run(self, user_input):
+    return self.llm_chain.run(human_input= user_input, history= self.memory.chat_memory.messages,stop= 'Human:')
